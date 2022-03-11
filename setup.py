84db544f31c86bf35da4c8883f674ac4f530e16d
Xavier Baró <xavierbaro@gmail.com>
2022-03-11 12:00:35 +0100

    Fixed use of python cannonical names

--- setup.py
+++ setup.py
@@ -12,9 +12,11 @@ with open("requirements.txt", "r") as fh:
         # Skip comments and optional requirements
         if not req.strip().startswith('#') and len(req.strip()) > 0 and not req.strip().startswith(
                 'mysqlclient') and not req.strip().startswith('psycopg2'):
-            # FIX error with native package name
+            # FIX error with native package names
             if req.startswith('zope-'):
                 req = req.replace('zope-', 'zope.')
+            if req.startswith('ruamel-'):
+                req = req.replace('-', '.')
             requirements.append(req)
 
 with open(os.path.join("src", "tesla_ce", "lib", "data", "VERSION"), "r") as fh:
