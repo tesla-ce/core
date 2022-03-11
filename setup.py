8ea0633474980a524f7a751a5076c22905136dc3
Xavier Baró <xavierbaro@gmail.com>
2022-03-11 13:06:45 +0100

    Python 3.6 deprecation

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Operating System :: POSIX :: Linux",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     include_package_data=True,
     install_requires=requirements,
     extras_require={
