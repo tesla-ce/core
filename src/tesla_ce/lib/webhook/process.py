47b6db440cd4acf3c6fdc80d58ebeab1831bb8b4
Roger <roger@rcod.net>
2023-07-28 13:36:38 +0200

    add code in webhook

--- src/tesla_ce/lib/webhook/process.py
+++ src/tesla_ce/lib/webhook/process.py
@@ -17,15 +17,15 @@ def process_webhook_message(message_id, provider_id):
 
 def process_webhook_message_tpt(webhook_message, provider):
     body = json.loads(webhook_message.body)
-    print(webhook_message)
 
     if body['action'] == 'UPDATE_RESULT':
         request = Request.objects.get(pk=body['request_id'])
-        request_provider_result = RequestProviderResult.objects.filter(provider=provider, request=request).first()
+        request_provider_result = RequestProviderResult.objects.get(provider=provider, request=request)
         request_provider_result.result = float(body['result'])
         request_provider_result.audit_data = body['audit_data']
         # status processed
         request_provider_result.status = 1
+        request_provider_result.code = body['code']
         request_provider_result.audit.save('{}__audit.json'.format(request_provider_result.request.data.name),
                                 ContentFile(simplejson.dumps(request_provider_result.audit_data).encode('utf-8')))
 
